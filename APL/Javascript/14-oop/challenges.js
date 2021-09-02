9911aa3915885f83c0492ed88928e4948cb984b4
Olegas Sevcenko <sevcenko.olegas@gmail.com>
2021-09-02 14:03:52 +0200

    Coding challenge 1

--- APL/Javascript/14-oop/challenges.js
+++ APL/Javascript/14-oop/challenges.js
@@ -10,3 +10,27 @@ DATA CAR 1: 'BMW' going at 120 km/h
 DATA CAR 2: 'Mercedes' going at 95 km/h
 GOOD LUCK 😀
 */
+
+
+const Car = function (make, speed) {
+    this.make = make
+    this.speed = speed
+}
+
+Car.prototype.accelerate = function() {
+    this.speed += 10
+    console.log(this.speed);
+}
+
+Car.prototype.brake = function() {
+    this.speed -= 5
+    console.log(this.speed);
+}
+
+const bmw = new Car('BMW', 120)
+const mercedes = new Car('Mercedes', 95)
+bmw.accelerate()
+bmw.accelerate()
+bmw.accelerate()
+bmw.brake()
+console.log(bmw);
