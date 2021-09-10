93f0e8e7a702ee6e4742db2a45c2c3ce39087862
Olegas Sevcenko <sevcenko.olegas@gmail.com>
2021-09-10 11:39:07 +0200

    Done with OOP section, encapsulation, chaining

--- APL/Javascript/14-oop/challenges.js
+++ APL/Javascript/14-oop/challenges.js
@@ -93,6 +93,7 @@ console.log(ford);
 DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
 GOOD LUCK 😀
 */
+/* 
 
 const Car = function (make, speed) {
     this.make = make
@@ -135,3 +136,58 @@ tesla.accelerate()
 tesla.brake()
 
 console.log(tesla);
+*/
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+1. Re-create challenge #3, but this time using ES6 classes: create an 'EVCl' child class of the 'CarCl' class
+2. Make the 'charge' property private;
+3. Implement the ability to chain the 'accelerate' and 'chargeBattery' methods of this class, and also update the 'brake' method in the 'CarCl' class. They experiment with chining!
+DATA CAR 1: 'Rivian' going at 120 km/h, with a charge of 23%
+GOOD LUCK 😀
+*/
+
+
+class CarCl {
+    constructor(make, speed) {
+        this.make = make
+        this.speed = speed
+    }
+    accelerate() {
+        this.speed += 10
+        console.log(`${this.make} going at ${this.speed} km/h`);
+        return this;
+    }
+    brake() {
+        this.speed -= 5
+        console.log(`${this.make} going at ${this.speed} km/h`);
+        return this;
+    }
+}
+
+class EV extends CarCl {
+    #charge;
+    constructor(make, speed, charge) {
+        super(make, speed)
+        this.#charge = charge
+    }
+    chargeBattery(chargeTo) {
+        console.log(`Current battery ${this.#charge}% will be charged up to ${chargeTo}%`);
+        this.#charge += chargeTo
+        return this;
+    }
+    accelerate() {
+        this.speed += 20
+        this.#charge -= 1
+        console.log(`${this.make} going at ${this.speed} km/h, with a charge of ${this.#charge}%`);
+        return this;
+    }
+}
+
+
+const rivian = new EV('Rivian', 120, 23)
+console.log(rivian);
+rivian.accelerate().brake().brake().chargeBattery(50).brake()
+console.log(rivian);
